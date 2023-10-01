b717b7bbfd51b284055fa074e848640ebe8b8541
“aanchal2057” <aanchalneupane@gmail.com>
2023-10-01 18:05:14 +0545

    blogpage

---
+++ final_Project/components/Post/SectionPart.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const SectionPart = ({posts}) => {
+  return (
+    <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 mt-10">
+          <div className="flex items-center justify-between mb-4 ">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              Latest Blog Posts
+            </h5>
+          </div>
+          <div className="flow-root">
+            <ul
+              role="list"
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              {posts.map((post) => (
+                <li className="py-3 sm:py-4">
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-shrink-0">
+                      <img
+                        className="w-8 h-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"
+                        alt="Author image"
+                      />
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        {post.author}
+                      </p>
+                      <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                        {post.date}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <a
+                     href={`/posts/${post.id}`} 
+                      class="text-lg font-bold leading-6 text-gray-900 dark:text-white hover:underline"
+                    >
+                      {post.title}
+                    </a>
+                    <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
+                      {post.excerpt}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+  )
+}
+
+export default SectionPart
